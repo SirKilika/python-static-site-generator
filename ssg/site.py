b5968d79874cb58aaffbc87e662f9f6121e606d3
SirKilika <fabiomacielsilva87@gmail.com>
2023-02-12 14:34:37 +0000

    Update site.py

---
+++ ssg/site.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+class Site:
+    def __init__(self,source,dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self,path):
+        directory = self.dest/self.source.relative_to()
+        self.dest.mkdir(partens=True,exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True,exists_ok=True)
+        for path in self.source.rglob("*"):
+            if path.
