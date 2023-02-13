ce7ad5d41c28950d8d1b5dcdd2111c3a765593d9
SirKilika <fabiomacielsilva87@gmail.com>
2023-02-13 21:22:00 +0000

    Update content.py

--- ssg/content.py
+++ ssg/content.py
@@ -38,7 +38,7 @@ class Content(Mapping):
     def __len__(self):
         return len(self.data)
 
-    def __repr__():
+    def __repr__(self):
         data = {}
         for key,value in self.data.items():
             if key != "content":
